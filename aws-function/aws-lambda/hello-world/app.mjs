673ceccce58ce040e124c845045bbe2ed95a15bf
Tomislav Tipurić <tomislavt@hotmail.com>
2024-05-06 17:12:32 -0600

    Added AWS Lambda with SAM

---
+++ aws-function/aws-lambda/hello-world/app.mjs
@@ -0,0 +1,35 @@
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+
+export const lambdaHandler = async (event, context) => {
+    try {
+
+        // get the message from the http request query string parameter called message
+        const message = event.queryStringParameters.message;
+
+        // return the message in the http response
+        console.log(message);
+        console.log(event);
+        console.log(context);
+
+        return {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'Your message was: ' + message + '!',
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+};
